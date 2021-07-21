5dee153f60f2ffe1aaa82f26d9f88eb2bfbc69c4
abdumamdouh <eabdogfx@gmail.com>
2021-07-21 02:05:26 +0200

    Split BookSearch Component into Two Components

---
+++ src/components/SearchResult.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class SearchResult extends Component {
+  render() {
+    return (
+      <div>
+        <div className="search-books-results">
+          <ol className="books-grid" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SearchResult;
